e679a4b366abc34bdc13425c945e0e50cd3aba36
Chetan700 <96438303+Chetan700@users.noreply.github.com>
2024-11-30 07:03:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,9 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Credit Tg - @officialchetanjoshi
+# Ask Doubt on telegram @officialchetanjoshi
 
 from main import Bot
 
 Bot().run()
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Credit Tg - @officialchetanjoshi
+# Ask Doubt on telegram @officialchetanjoshi
